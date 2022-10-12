30585664fb5d7084d84f4cbd6c9c08f2c317b78f
Farid Amimri <farid.amimri@gmail.com>
2022-10-12 18:15:59 +0100

    Setting up useState in typeScript + add handleChange and addTask fn

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,47 @@
 /** @format */
 
-import React, { FC } from 'react'
+import React, { ChangeEvent, FC, useState } from 'react'
 import './App.css'
+import {ITask} from './Interfaces'
 
 const App: FC = () => {
+  const [task, setTask] = useState<string>('')
+  const [deadline, setDeadline] = useState<number>(0)
+  const [todoList, setTodoList] = useState<ITask[]>([])
+
+  function handleChange (event: ChangeEvent<HTMLInputElement>): void {
+    if (event.target.name === 'task') {
+      setTask(event.target.value)
+    } else {
+      setDeadline(Number(event.target.value))
+    }
+  }
+
+  function addTask (): void {
+    const newTask = { task, deadline}
+    setTodoList([...todoList, newTask])
+    console.log(todoList);
+    
+  }
+
   return (
     <div className='App'>
       <div className='header'>
         <div className='inputContainer'>
-          <input type='text' placeholder='Task' />
-          <input type='number' placeholder='Deadline' />
+          <input
+            type='text'
+            placeholder='Task'
+            name='task'
+            onChange={handleChange}
+          />
+          <input
+            type='number'
+            placeholder='Deadline'
+            name='deadline'
+            onChange={handleChange}
+          />
         </div>
-        <button>Add Task</button>
+        <button onClick={addTask}>Add Task</button>
       </div>
       <div className='todoList'></div>
     </div>
