becf0d92c8fcb9d050960e1f9872945bb54c4e44
Farid Amimri <farid.amimri@gmail.com>
2022-10-13 13:02:10 +0100

    Add map fn on todoList to render tasks

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@
 
 import React, { ChangeEvent, FC, useState } from 'react'
 import './App.css'
-import {ITask} from './Interfaces'
+import TodoTask from './Components/TodoTask'
+import { ITask } from './Interfaces'
 
 const App: FC = () => {
   const [task, setTask] = useState<string>('')
   const [deadline, setDeadline] = useState<number>(0)
   const [todoList, setTodoList] = useState<ITask[]>([])
 
-  function handleChange (event: ChangeEvent<HTMLInputElement>): void {
+  function handleChange(event: ChangeEvent<HTMLInputElement>): void {
     if (event.target.name === 'task') {
       setTask(event.target.value)
     } else {
@@ -17,8 +18,8 @@ const App: FC = () => {
     }
   }
 
-  function addTask (): void {
-    const newTask = { task, deadline}
+  function addTask(): void {
+    const newTask = { task, deadline }
     setTodoList([...todoList, newTask])
     setTask('')
     setDeadline(0)
@@ -45,7 +46,11 @@ const App: FC = () => {
         </div>
         <button onClick={addTask}>Add Task</button>
       </div>
-      <div className='todoList'></div>
+      <div className='todoList'>
+        {todoList.map((item: ITask, key: number) => {
+          return <TodoTask key={key} task={item} />
+        })}
+      </div>
     </div>
   )
 }
